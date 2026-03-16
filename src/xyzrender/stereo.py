66af772b6d4ebb1acf9727960ca0466e4592502c
James <144615809+JamesOBrien2@users.noreply.github.com>
2026-03-16 23:08:01 +0000

    Read axial/planar stereo annotations from xyzgraph

--- src/xyzrender/stereo.py
+++ src/xyzrender/stereo.py
@@ -33,12 +33,28 @@ def build_stereo_annotations(
 
     if include_rs:
         for idx, data in graph.nodes(data=True):
-            label = data.get("stereo")
+            label = data.get("stereo_rs") or data.get("stereo")
             if label in {"R", "S"}:
                 annotations.append(AtomValueLabel(idx, label, on_atom=(rs_style == "atom")))
     if include_ez:
         for i, j, data in graph.edges(data=True):
-            label = data.get("stereo")
+            label = data.get("stereo_ez") or data.get("stereo")
             if label in {"E", "Z"}:
                 annotations.append(BondLabel(i, j, label))
+        for i, j, data in graph.edges(data=True):
+            label = data.get("stereo_axial")
+            if label in {"Rₐ", "Sₐ"}:
+                annotations.append(BondLabel(i, j, label))
+        for i, j, data in graph.edges(data=True):
+            label = data.get("stereo_planar")
+            if label in {"Rₚ", "Sₚ"}:
+                annotations.append(BondLabel(i, j, label))
+        for axis in graph.graph.get("stereo_axes", []):
+            try:
+                i = int(axis["i"])
+                j = int(axis["j"])
+                label = str(axis["label"])
+            except Exception:
+                continue
+            annotations.append(BondLabel(i, j, label))
     return annotations
